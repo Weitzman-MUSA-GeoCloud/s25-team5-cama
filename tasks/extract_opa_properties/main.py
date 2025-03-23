6f5f01445ea3b4e4911d324583070bbc1c27fa24
weijingg <97523934+weijingg@users.noreply.github.com>
2025-03-23 18:59:58 -0400

    extract and prepare opa properties
    - Created .gitcloudignore
    - Created main.py and requirements.txt to extract opa properties in task folder
    - Created main.py and requirements.txt to prepare opa properties in task folder
    - These scripts are for testing deployment locally

---
+++ tasks/extract_opa_properties/main.py
@@ -0,0 +1,41 @@
+import os
+import pathlib
+import requests
+import functions_framework
+from google.cloud import storage
+from dotenv import load_dotenv
+load_dotenv()
+
+DATA_DIR = pathlib.Path(__file__).parent
+
+
+@functions_framework.http
+def extract_opa_properties(request):
+
+    print('Extracting OPA Properties data')
+
+    # Download the OPA Properties data as a CSV
+    url = 'https://opendata-downloads.s3.amazonaws.com/opa_properties_public.csv'
+    filename = DATA_DIR / 'opa_properties.csv'
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as f:
+        f.write(response.content)
+
+    print(f'Downloaded {filename}')
+
+    # Upload the downloaded file to cloud storage
+    bucket_name = os.getenv('DATA_LAKE_BUCKET_RAW')
+    blobname = 'opa_properties/opa_properties.csv'
+
+    # For now we will overwrite the file each time we run the script
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blobname)
+    blob.upload_from_filename(filename)
+
+    print(f'Uploaded {blobname} to {bucket_name}')
+
+    return f'Downloaded and uploaded gs://{bucket_name}/{blobname}'
