a6e72f142ae1ae874e575fb2bf32360f8010cdfa
weijingg <97523934+weijingg@users.noreply.github.com>
2025-03-27 21:24:33 -0400

    Updated load scripts
    - Edited all 3 load scripts to read external sql file
    - Added 3 sql files for the load step: source opa_properties, opa_assessments, and pwd_parcels

--- tasks/load_opa_properties/main.py
+++ tasks/load_opa_properties/main.py
@@ -11,29 +11,37 @@ DATA_DIR = pathlib.Path(__file__).parent
 @functions_framework.http
 def load_opa_properties(request):
 
+    # Read the SQL file specified in the request (from query string paramter)
+    sql_path = DATA_DIR / 'source_opa_properties.sql'
+
+    # Check that the file exists (else return 404 response from function i.e. not found)
+    if (not sql_path.exists()) or (not sql_path.is_file()):
+        # Return a 404 (not found) response if not
+        return f'File {sql_path} not found', 404
+
+    # Read the SQL file (if exists)
     print('Extracting OPA Properties data')
 
-    bucket_name = os.getenv('DATA_LAKE_BUCKET_PREPARE')
-    dataset_name = os.getenv('DATA_LAKE_DATASET')
+    with open(sql_path, 'r', encoding='utf-8') as sql_file:  # encoding is utf8
+        sql_query_template = sql_file.read()  # read file as a template because of ${} placeholders
+        sql_query = render_template(  # process template with special functions
+            sql_query_template,
+            {
+                'bucket_name': os.getenv('DATA_LAKE_BUCKET_PREPARE'),
+                'dataset_name': os.getenv('DATA_LAKE_DATASET'),
+                'prepared_blobname': 'opa_properties/data.jsonl',
+                'table_name': 'opa_properties'
+            }
+        )
 
-    # Load the data into BigQuery as an external table
-    prepared_blobname = 'opa_properties/data.jsonl'
-    table_name = 'opa_properties'
-    table_uri = f'gs://{bucket_name}/{prepared_blobname}'
-
-    create_table_query = f'''
-    CREATE OR REPLACE EXTERNAL TABLE {dataset_name}.{table_name}
-    OPTIONS(
-        format = 'JSON',
-        uris = ['{table_uri}']
-    )
-    '''
-    print(create_table_query)
-
+    # Run the SQL query
     bigquery_client = bigquery.Client()
-    # print(bigquery_client._credentials.service_account_email)
-    bigquery_client.query_and_wait(create_table_query)
+    bigquery_client.query_and_wait(sql_query)
 
-    print(f'Loaded {table_uri} into {dataset_name}.{table_name}')
+    print(f'Ran the SQL file {sql_path} to load table')
 
-    return f'Loaded {table_uri} into {dataset_name}.{table_name}'
+    return f'Ran the SQL file {sql_path} to load table'
+
+def render_template(sql_query_template, context):
+    clean_template = sql_query_template.replace('${', '{')
+    return clean_template.format(**context)
