040fcc223bdcfb2dd33049da68c60af013f6c209
weijingg <97523934+weijingg@users.noreply.github.com>
2025-04-08 16:59:16 -0400

    load vector tiles

--- ui/reviewer/js/main.js
+++ ui/reviewer/js/main.js
@@ -1,8 +1,48 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3lsdmlhdXBlbm4iLCJhIjoiY20weTdodGpiMGt4MDJsb2UzbzZnd2FmMyJ9.H6mn-LOHFUdv7swHpM7enA'
+// Using Maplibre GL
+var map = new maplibregl.Map({
+  container: 'map', // container id
+  style: 'https://tiles.stadiamaps.com/styles/alidade_smooth.json', // Stadia
+  center: [-75.1652, 39.9526],  // [lng, lat]
+  zoom: 12
+});
 
-const map = new mapboxgl.Map({
-    container: 'map', 
-    style: 'mapbox://styles/mapbox/light-v11',
-    center: [-75.1652, 39.9526], 
-    zoom: 12, 
+map.on('load', () => {
+  map.addSource('properties', {
+      type: 'vector',
+      tiles: [
+          'https://storage.googleapis.com/musa5090s25-team5-public/tiles/properties/{z}/{x}/{y}.pbf'
+      ],
+      'minzoom': 12,
+      'maxzoom': 20
   });
+  map.addLayer({
+      'id': 'properties-fill',
+      'type': 'fill',
+      'source': 'properties',
+      'source-layer': 'property_tile_info',
+      'paint': {
+          'fill-color': [
+            'interpolate',
+            ['linear'],
+            ['get', 'tax_year_assessed_value'],
+            0, '#f0f9e8',
+            500000, '#bae4bc',
+            1000000, '#7bccc4',
+            5000000, '#2b8cbe',
+            10000000, '#08589e'
+        ],
+      'fill-opacity': 0.4
+      }
+  });
+});
+
+// Mapbox does not work with Maplibre GL, which works better with vector tiles
+
+// mapboxgl.accessToken = 'pk.eyJ1Ijoic3lsdmlhdXBlbm4iLCJhIjoiY20weTdodGpiMGt4MDJsb2UzbzZnd2FmMyJ9.H6mn-LOHFUdv7swHpM7enA'
+
+// const map = new mapboxgl.Map({
+//     container: 'map', 
+//     style: 'mapbox://styles/mapbox/light-v11',
+//     center: [-75.1652, 39.9526], 
+//     zoom: 12, 
+//   });
