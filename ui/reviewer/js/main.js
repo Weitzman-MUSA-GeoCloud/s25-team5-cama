ecd9119456f3eeac3e4df45641113855dc1d3230
laivlys <lamsylviazining@gmail.com>
2025-04-09 16:35:27 -0400

    Creating the search bar and the filter button

--- ui/reviewer/js/main.js
+++ ui/reviewer/js/main.js
@@ -35,14 +35,3 @@ map.on('load', () => {
       }
   });
 });
-
-// Mapbox does not work with Maplibre GL, which works better with vector tiles
-
-// mapboxgl.accessToken = 'pk.eyJ1Ijoic3lsdmlhdXBlbm4iLCJhIjoiY20weTdodGpiMGt4MDJsb2UzbzZnd2FmMyJ9.H6mn-LOHFUdv7swHpM7enA'
-
-// const map = new mapboxgl.Map({
-//     container: 'map', 
-//     style: 'mapbox://styles/mapbox/light-v11',
-//     center: [-75.1652, 39.9526], 
-//     zoom: 12, 
-//   });
