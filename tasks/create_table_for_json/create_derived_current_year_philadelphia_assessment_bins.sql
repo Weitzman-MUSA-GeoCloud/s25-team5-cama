1a8ca26472ab318ed5a72bbada79a2d8a37e000b
YaohanXu <xuyaohan99@gmail.com>
2025-04-15 20:06:06 -0400

    update folder structure for whole workflow

--- tasks/create_table_for_json/create_derived_current_year_philadelphia_assessment_bins.sql
+++ tasks/create_table_for_json/create_derived_current_year_philadelphia_assessment_bins.sql
@@ -1,22 +1,10 @@
--- Group properties using meaningful value ranges that reflect Philadelphia’s market structure, policy relevance, and housing types.
+-- Group 2025 property assessments using custom value ranges based on Philly’s housing market and tax policy.
 CREATE OR REPLACE TABLE derived.current_year_philadelphia_assessment_bins AS
 WITH labeled AS (
   SELECT
-    year AS tax_year,
+    2025 AS tax_year,
     CASE
-      WHEN market_value <= 100000 THEN 1
-      WHEN market_value <= 200000 THEN 2
-      WHEN market_value <= 300000 THEN 3
-      WHEN market_value <= 400000 THEN 4
-      WHEN market_value <= 600000 THEN 5
-      WHEN market_value <= 800000 THEN 6
-      WHEN market_value <= 1000000 THEN 7
-      WHEN market_value <= 2000000 THEN 8
-      WHEN market_value <= 5000000 THEN 9
-      ELSE 10
-    END AS bin,
-    CASE
-      WHEN market_value <= 100000 THEN 0
+      WHEN market_value > 0 AND market_value <= 100000 THEN 0
       WHEN market_value <= 200000 THEN 100000
       WHEN market_value <= 300000 THEN 200000
       WHEN market_value <= 400000 THEN 300000
@@ -28,7 +16,7 @@ WITH labeled AS (
       ELSE 5000000
     END AS lower_bound,
     CASE
-      WHEN market_value <= 100000 THEN 100000
+      WHEN market_value > 0 AND market_value <= 100000 THEN 100000
       WHEN market_value <= 200000 THEN 200000
       WHEN market_value <= 300000 THEN 300000
       WHEN market_value <= 400000 THEN 400000
@@ -40,7 +28,7 @@ WITH labeled AS (
       ELSE 500000000
     END AS upper_bound
   FROM core.opa_assessments
-  WHERE market_value > 0 AND year = 2024
+  WHERE market_value > 0
 ),
 aggregated AS (
   SELECT
@@ -55,6 +43,5 @@ SELECT *
 FROM aggregated
 ORDER BY lower_bound;
 
--- Next Steps:
--- 1. Does the legend of the map need to match the bins here?
--- 2. Create a table for different neighborhoods (group by neighborhoods)? Differentiate the file name of Philadelphia and Neighborhoods.
+-- 1. Confirm the table name of current year property assessments.
+-- 2. Confirm the field name of property assessment value.
